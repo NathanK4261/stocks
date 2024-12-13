1038cd249d9eb3d70a5a2d4e58e484c4f4be8445
Nathan <nkeidel00@gmail.com>
2024-12-13 12:48:25 -0800

    added info comments and included required package

--- condasetup/setup.sh
+++ condasetup/setup.sh
@@ -1,7 +1,10 @@
+# NOTE: Call this using "source"
+#	EXAMPLE: `source setup.sh`
 conda create -n stocks
 conda activate stocks
 conda install pytorch -c pytorch-nightly
 conda install anaconda::pandas -y
+conda install openpyxl -y
 conda install pytz -y
 conda install scikit-learn -y
 conda install requests -y
