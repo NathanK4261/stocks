966caba81a01d24b056a2703e55156b047c1ecd2
Nathan <nkeidel00@gmail.com>
2025-01-06 13:46:49 -0800

    first untested update to new protocall with `modules.tickers` class

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
-from modules.internet import TiingoClient, NewsWebScraper
+from modules.internet import YahooClient, NewsWebScraper
 from modules.llm import LlamaChat
+
+import modules.tickers
 
 from datetime import datetime, date
 import time
@@ -13,8 +15,11 @@ import json
 import logging
 
 def is_weekend(date):
+	'''
+	Returns true if the current day is a weekend
+	'''
 	try: 
-		# Use isoweekday() to get the weekday (Monday is 1 and Sunday is 7)
+		# Monday is 1 and Sunday is 7
 		day_of_week = (date.weekday() + 1) % 7  # Convert Sunday from 6 to 0
 			
 		# Determine if it's a weekday or a weekend
@@ -34,8 +39,8 @@ logging.basicConfig(filename='database.log', encoding='utf-8', level=logging.WAR
 with open('config.json') as f:
 	config = json.load(f)
 
-# Create a TiingoClient
-tiingo_client = TiingoClient()
+# Create a YahooClient
+yahoo_client = YahooClient()
 
 # Set up web scraper
 scraper = NewsWebScraper()
@@ -48,57 +53,72 @@ def daily_protocall():
 	Collects data on the specified ticker and analyzes it
 	'''
 
-	# Get the daily market data
-	df = tiingo_client.get_daily_market_data()
+	# Iterate through every ticker in the S&P 500
+	for ticker in modules.tickers.TICKERS:
 
-	if df is None:
-		return False # Exit the protocall if there is no data collected
+		# Get the daily market data
+		current_data = yahoo_client.current(ticker)
 
-	# Collect news information
-	scraped_sites = scraper.scrape_from_yf()
+		if current_data is None:
+			return False # Exit the protocall if there is no data collected
 
-	# Create a variable to store the average sentiment
-	sentiments = []
+		# Collect news information
+		scraped_sites = scraper.scrape_from_yf(ticker)
 
-	for site in scraped_sites:
-		# Create a prompt to get the sentiment of the stock
-		sentiment_prompt = f'''
-Imagine you are a human who is hoping to buy {config['TICKER']} stock.
-You want to read articles, and rate your interest in buying {config['TICKER']}
-on a scale from 1-10 based on the readings of the article. A "1" is equivalent to no
-interest in buying {config['TICKER']}, and a "10" means you have an 100% interest
-in buying {config['TICKER']}.
+		if scraped_sites is None:
+			return False
 
-Return only a number between 1-10, and nothing else.
+		# Create a variable to store the average sentiment
+		sentiments = []
 
-Article below:
+		for site in scraped_sites:
+			# Create a prompt to get the sentiment of the stock
+			sentiment_prompt = f'''
+				Imagine you are a human who is hoping to buy {ticker} stock.
+				You want to read articles, and rate your interest in buying {ticker}
+				on a scale from 1-10 based on the readings of the article. A "1" is equivalent to no
+				interest in buying {ticker}, and a "10" means you have an 100% interest
+				in buying {ticker}.
 
-{site.content}
-'''
+				You also want to to ignore as much advertising text as possible within the article, as this information is
+				irrelevant to the analysis of the performance of {ticker}.
 
-		# Store the sentiment in a list so we can average the sentiment over all stock
+				If enough information about the sentiment can be derived, return only a number between 1-10, and nothing else.
+
+				If enough information about the sentiment cannot be derived, return the word "NONE" and nothing else.
+
+				Article below:
+
+				{site.title}
+
+				{site.content}
+			'''
+
+			# Store the sentiment in a list so we can average the sentiment over all stock
+			try:
+				sentiments.append(int(llama.prompt(sentiment_prompt)))
+			except ValueError:
+				pass
+
+		# Get the average sentiment of the stock for today
+		avg_sentiment = mean(sentiments)
+
+		# Try and load saved historical data
 		try:
-			sentiments.append(int(llama.prompt(sentiment_prompt)))
-		except ValueError:
-			pass
+			historical_data = pd.read_pickle(f'stockdata/{ticker}.pkl')
+		except:
+			# If no historical data exists, start storing
+			# historical data from today
+			historical_data = current_data
 
-	# Get the average sentiment of the stock for today
-	avg_sentiment = mean(sentiments)
+		# Create a new column to add the market sentiment
+		historical_data['sentiment'] = avg_sentiment
 
-	# Try and load saved historical data
-	try:
-		historical_data = pd.read_pickle(f'stockdata/{config['TICKER']}/{config['TICKER']}.pkl')
-	except:
-		# If no historical data exists, start storing
-		# historical data from today
-		historical_data = df
+		# Save the dataframe as a file
+		historical_data.to_pickle(f'stockdata/{ticker}.pkl')
 
-	# Create a new column to add the market sentiment
-	historical_data['sentiment'] = avg_sentiment
-
-	# Save the dataframe as a file
-	historical_data.to_pickle(f'stockdata/{config['TICKER']}/{config['TICKER']}.pkl')
-	historical_data.to_csv(f'stockdata/{config['TICKER']}/{config['TICKER']}.csv', index=False)
+		# Test option: save to csv to get visual representation
+		historical_data.to_csv(f'stockdata/csv/{ticker}.csv')
 
 	# Save last update time in config
 	with open('config.json', 'w') as f:
@@ -110,8 +130,11 @@ Article below:
 
 # Main logic
 def main():
+	# Log the bootup time
+	logger.warning(f'{str(date.today())} @ {datetime.now().hour}:{datetime.now().minute} - BOOT')
+
 	try:
-		# Start if market has been closed for an hour (since we want todays data as well)
+		# Start only if market has been closed for an hour (since we want todays data as well)
 		while not (datetime.now(pytz.timezone('US/Eastern')).hour < 9 or \
 			(datetime.now(pytz.timezone('US/Eastern')).hour >= 17)):
 				pass
