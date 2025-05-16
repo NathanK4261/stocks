3a0acd587c031e49b98c0d3907d96abe97d4afe9
Nathan <nkeidel00@gmail.com>
2025-05-15 20:30:42 -0700

    added check for compatability with `YahooClient` class

--- modules/tickers.py
+++ modules/tickers.py
@@ -5,8 +5,12 @@ A list containing the ticker symbols of every stock in the S&P 500
 Yes, it does update the list every time...
 '''
 from pandas import read_excel
+from internet import YahooClient as yc
 
 TICKERS = []
+
+# A list of tickers not allowed to be used
+banned_tickers = ['CASH_USD', '-', 'BRK.B', 'BRK.A']
 
 
 # Updates "TICKERS" to include the latest companies in the S&P 500
@@ -16,5 +20,12 @@ url = 'https://www.ssga.com/us/en/intermediary/etfs/library-content/products/fun
 holdings = read_excel(url, engine='openpyxl', skiprows=4).dropna()['Ticker']
 
 for ticker in holdings:
-	if ticker != 'CASH_USD' or ticker != '-':
+	if ticker not in banned_tickers:
 		TICKERS.append(ticker)
+
+# Double check each ticker works with `YahooClient`
+c = yc()
+
+for t in TICKERS:
+	if c.current(t) == 1:
+		TICKERS.remove(t)
