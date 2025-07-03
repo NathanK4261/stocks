a95d2e0e52115b37fd9eef221bc22f97a4cdfd56
Nathan <nkeidel00@gmail.com>
2025-07-02 18:59:50 -0700

    minor changes

--- modules/tickers.py
+++ modules/tickers.py
@@ -5,11 +5,12 @@ A list containing the ticker symbols of every stock in the S&P 500
 Yes, it does update the list every time...
 '''
 from pandas import read_excel
+from .internet import YahooClient as yc
 
 TICKERS = []
 
 # A list of tickers not allowed to be used
-banned_tickers = ['CASH_USD', '-']
+banned_tickers = ['CASH_USD', '-', 'BRK.B', 'BRK.A']
 
 
 # Updates "TICKERS" to include the latest companies in the S&P 500
