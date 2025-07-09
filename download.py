562085bc68fb0478befcc11f998e6ad4e631b670
Nathan <nkeidel00@gmail.com>
2025-07-08 17:12:13 -0700

    added some code to check if hisotircal data already exists for the current day befreo you pull from the API. Reworked the code to align with this change, and also created check to make sure `LAST_PROTOCALL_UPDATE` only updates if there was no keyboard interrupt.

--- download.py
+++ download.py
@@ -48,6 +48,22 @@ def run_protocall(ticker: str):
 	Collects data on the specified ticker, analyzes it, and saves it as a pandas dataframe
 	'''
 
+	# Check to see if data was already collected for the current day
+	try:
+		# Read from historical data
+		historical_data = pd.read_pickle(f'stockdata/{ticker}.pkl')
+
+		# Compare today's date to the latest saved date in the historical data
+		if str(date.today()) == historical_data['date'][len(historical_data) - 1]:
+			
+			# If there is already data for the current day, ignore pulling data for today
+			logger.warning(log_msg(f'{ticker} - Data already exists for curent day'))
+
+			return True
+	except FileNotFoundError:
+		# We can ignore this error as it just means that there is no saved data on the specific ticker
+		pass
+
 	# Get the daily market data
 	result, current_data = yahoo_client.current(ticker)
 
@@ -62,7 +78,7 @@ def run_protocall(ticker: str):
 	if result == 1: # 1 = Error
 
 		# If there is no news data, set `scraped_sites` to an empty list
-		logger.warning(log_msg(scraped_sites))
+		logger.error(log_msg(scraped_sites))
 
 		scraped_sites = []
 	else:
@@ -74,32 +90,25 @@ def run_protocall(ticker: str):
 			temp.append([site.title, site.content])
 
 		scraped_sites = temp
-
+	
 	# Try and load saved historical data
 	try:
 		historical_data = pd.read_pickle(f'stockdata/{ticker}.pkl')
 
-		# Check if data already exists for the current day
-		if current_data['date'][0] == historical_data['date'][len(historical_data) - 1]:
-			
-			# If there is already data for the current day, ignore the pulled data
-			logger.warning(log_msg(f'{ticker} - Data already exists for curent day'))
+		# Add our list of "NewsWebPage" objects to our current dataframe
+		current_data['news'] = [scraped_sites]
 
-			return True
+		# If there is not data for the current day, make a new entry
+		historical_data = pd.concat([historical_data, current_data], ignore_index=True)
 
-		else:
-			# Add our list of "NewsWebPage" objects to our current dataframe
-			current_data['news'] = [scraped_sites]
-
-			# If there is not data for the current day, make a new entry
-			historical_data = pd.concat([historical_data, current_data], ignore_index=True)
-
-	except Exception as e:
-		# If no historical data exists, start storing historical data from today
-		historical_data = current_data
+	# If no historical data exists, start storing historical data from today
+	except FileNotFoundError:
 
 		# Add our list of "NewsWebPage" objects to our current dataframe
 		current_data['news'] = [scraped_sites]
+
+		# Set our historical dataframe to our current dataframe
+		historical_data = current_data
 
 	# Save the dataframe as a file
 	historical_data.to_pickle(f'stockdata/{ticker}.pkl')
@@ -137,13 +146,14 @@ if str(date.today()) != config['LAST_PROTOCALL_UPDATE'] and market_open():
 	logger.warning(log_msg(f'START PROTO ({len(modules.tickers.TICKERS)} Tickers)'))
 
 	# Set a boolean in case we need to KeyboardInterrupt
-	run = True
+	save_data = True
 
 	for ticker in modules.tickers.TICKERS:
 	
-		while run:
+		while True:
 			try:
 				# Iterate through every ticker
+				
 				#logger.warning(log_msg(f'{ticker.upper()} - PULL'))
 
 				protocall_complete = run_protocall(ticker)
@@ -157,13 +167,14 @@ if str(date.today()) != config['LAST_PROTOCALL_UPDATE'] and market_open():
 
 			except KeyboardInterrupt:
 				logger.warning(log_msg('KeyboardInterrupt'))
-				run = False
+				save_data = False
 
-	# Save last update time in config
-	with open('config.json', 'w') as f:
-		config['LAST_PROTOCALL_UPDATE'] = str(date.today())
+	# Save last update time in config, but only if "save_data" is set to "True"
+	if save_data:
+		with open('config.json', 'w') as f:
+			config['LAST_PROTOCALL_UPDATE'] = str(date.today())
 
-		json.dump(config, f, indent=4)
+			json.dump(config, f, indent=4)
 
 	# Log when daily protocall ends
 	logger.warning(log_msg('END PROTO'))
