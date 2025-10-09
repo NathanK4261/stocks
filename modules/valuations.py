2c53307a8d5861071b9b95ae4d128a23162423c9
Nathan <nkeidel00@gmail.com>
2025-10-08 18:22:16 -0700

    created new file to store valuation metrics to be obtained

---
+++ modules/valuations.py
@@ -0,0 +1,42 @@
+'''
+# valuations
+
+A list that stores the name of each valuation metric in `yfinance`
+'''
+
+yf_values = [
+	'industry',
+	'sector',
+	'previousClose',
+	'currentPrice',
+	'open',
+	'dayLow',
+	'dayHigh',
+	'beta',
+	'trailingPE',
+	'forwardPE',
+	'volume',
+	'averageVolume',
+	'averageVolume10days',
+	'bid',
+	'ask',
+	'marketCap',
+	'fiftyTwoWeekLow',
+	'fiftyTwoWeekHigh',
+	'priceToSalesTrailing12Months',
+	'fiftyDayAverage',
+	'twoHundredDayAverage',
+	'profitMargins',
+	'shortRatio',
+	'bookValue',
+	'priceToBook',
+	'earningsQuarterlyGrowth',
+	'epsTrailingTwelveMonths',
+	'epsForward',
+	'enterpriseToRevenue',
+	'quickRatio',
+	'currentRatio',
+	'returnOnAssets',
+	'returnOnEquity',
+	'trailingPegRatio',
+]
