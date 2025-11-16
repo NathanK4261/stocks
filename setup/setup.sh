a2132e26af265c726c268acfa71113f429ea3d6c
Nathan <nkeidel00@gmail.com>
2025-11-16 14:25:23 -0800

    removed some libraries i will never use, added tqdm install

--- setup/setup.sh
+++ setup/setup.sh
@@ -12,13 +12,11 @@ conda install scikit-learn -y
 conda install requests -y
 #conda install onnx -y
 conda install pip -y
-#conda install flask -y
+conda install tqdm -y
 #pip install --upgrade onnxscript
 pip install --upgrade ollama
 pip install --upgrade yfinance
 pip install --upgrade pandas-market-calendars
-#pip install --upgrade newspaper3k
-#pip install --upgrade lxml_html_clean
 pip install --upgrade streamlit
 pip install --upgrade streamlit-scrollable-textbox
 pip install --upgrade plotly
